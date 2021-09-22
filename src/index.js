a89247ab5d76ec4c806991c2ad29826409743169
AmineHLub <Amino.bhl@gmail.com>
2021-09-22 09:49:30 +0100

    Add elements dynamically

--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
-// eslint-disable-next-line no-unused-vars
-import _ from 'lodash';
+/* eslint-disable no-unused-vars */
+import _, { template } from 'lodash';
 import './style.css';
+import trippledotsicon from './assets/3dots.png';
 
-// function component() {
-// const element = document.createElement('div');
+const arrOftodos = [
+  {
+    description: 'template1',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'template2',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'template3',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'template4',
+    completed: false,
+    index: 3,
+  },
+];
 
-// Lodash, currently included via a script, is required for this line to work
-// element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-// return element;
-// }
-
-// document.body.appendChild(component());
+for (let i = 0; i < arrOftodos.length; i += 1) {
+  const item = document.createElement('li');
+  const checkbox = document.createElement('input');
+  const toDo = document.createElement('input');
+  const trippleDotsMenu = document.createElement('a');
+  const trippleDotsMenuLogo = document.createElement('img');
+  document.querySelector('.todo-list-activities > ul').appendChild(item);
+  item.appendChild(checkbox);
+  checkbox.classList.add('checkbox');
+  checkbox.type = 'checkbox';
+  item.appendChild(toDo);
+  toDo.classList.add('list-items');
+  toDo.classList.add('noborder-input');
+  toDo.type = 'text';
+  toDo.value = arrOftodos[i].description;
+  toDo.disabled = true; // must double click to modify
+  item.appendChild(trippleDotsMenu);
+  trippleDotsMenu.classList.add('list-menu');
+  trippleDotsMenu.appendChild(trippleDotsMenuLogo);
+  trippleDotsMenuLogo.src = trippledotsicon;
+}
